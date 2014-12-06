5ab0d3366bac4462c4414ca184e920dd3676363c
clemenorellana <clemen.orellana@unitec.edu>
2014-12-06 12:19:56 -0600

    CRUD tipos de carreras listo, solo faltan unas validaciones como mostrar los nombres de los academicLevel en el listado, falta refrescar los datos una vez que se agrego o edito, Falta hacer validaciones de confirmacion de si desea borrar

--- AccionLaboral/Scripts/App/Controllers/careersController.js
+++ AccionLaboral/Scripts/App/Controllers/careersController.js
@@ -12,7 +12,8 @@ angular.module("careersController", ['ngRoute', 'careersRepository'])
 .controller('careersCtrl', ['$scope', 'careersRepo', function ($scope, careersRepo) {
 
     $scope.careerList = [];
-
+    $scope.actionCareer = "";
+    
     careersRepo.getAcademicLevels().success(function (data) {
         $scope.Career_AcademicLevels = data;
     });
@@ -28,24 +29,66 @@ angular.module("careersController", ['ngRoute', 'careersRepository'])
             $scope.loading = false;
         });
 
-    
+    $scope.setActionCareer = function (action, index) {
+        debugger
+        $scope.actionCareer = action;
+        if (action == "add") {
+            $scope.career_modalTitle = "Agregar Carrera";
+            $scope.career_buttonName = "Agregar";
+        }
+        else {
+            $scope.career_modalTitle = "Editar Carrera";
+            $scope.career_buttonName = "Editar";
+            $scope.editCareer(index);
+        }
+    }
 
     $scope.editCareer = function (index) {
+        debugger
+        var careerToEdit = $scope.careerList[index];
+        $scope.Career_CareerId = careerToEdit.CareerId
+        $scope.Career_Name = careerToEdit.Name;
+        $scope.Career_AcademicLevel = careerToEdit.AcademicLevelId;
+    };
+
+    $scope.saveCareer = function () {
+        debugger
+        
+        var career;
+
+        if ($scope.actionCareer == "add") {
+            career = {
+                Name: $scope.Career_Name,
+                AcademicLevelId: $scope.Career_AcademicLevel.AcademicLevelId,
+                AcademicLevelName: $scope.Career_AcademicLevel.Name
+            };
+
+            careersRepo.insertCareer(function () {
+            }, career);
+
+            $scope.careerList.push(career);
+        }
+        else {
+            career = {
+                CareerId: $scope.Career_CareerId,
+                Name: $scope.Career_Name,
+                AcademicLevelId: $scope.Career_AcademicLevel.AcademicLevelId,
+                AcademicLevelName: $scope.Career_AcademicLevel.Name
+            };
+
+            careersRepo.updateCareer(function () {
+            }, career);
+              
+            //refresh table with la values edited
+        }
+
+        $scope.actionCareer = "";
+        $scope.Career_CareerId
+        $scope.Name = "",
+        $scope.Career_AcademicLevel = "";
         
     };
 
-    $scope.addNewCareer = function () {
-        var lang = {
-            Name: $scope.Name,
-            AcademicLevelId: $scope.Career_AcademicLevel.AcademicLevelId,
-            AcademicLevelName: $scope.Career_AcademicLevel.Name
-        };
-
-        $scope.careerList.push(lang);
-
-        $scope.Name = "",
-        $scope.AcademicLevelId = "";
-    };
     
     $scope.removeCareer = function (index) {
         $scope.careerList.splice(index, 1);
--- AccionLaboral/Scripts/App/Services/careerRepository.js
+++ AccionLaboral/Scripts/App/Services/careerRepository.js
@@ -9,6 +9,23 @@ accionLabControllers.factory('careersRepo', ['$http', function ($http) {
         getAcademicLevels: function (callback) {
             return $http.get('api/AcademicLevels');
         },
+        //method for insert
+        insertCareer: function (callback, career) {
+            var career = {
+                "Name": career.Name,
+                "AcademicLevelId": career.AcademicLevelId
+            };
+            return $http.post(url, career);
+        },
+        //method for update
+        updateCareer: function (callback, career) {
+            var career = {
+                "CareerId": career.CareerId,
+                "Name": career.Name,
+                "AcademicLevelId": career.AcademicLevelId
+            };
+            return $http.put(url + '/' + career.CareerId, career);
+        },
         //method for delete
         deleteCareer: function (callback, id) {
             return $http.delete(url + '/' + id);
