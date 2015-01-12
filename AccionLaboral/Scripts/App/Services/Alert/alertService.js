3a47e8e82cade8af386b9b49568281dffd4de578
Roger Paz <rogerpaz_16@unitec.edu>
2015-01-11 22:24:14 -0600

    Alertas

---
+++ AccionLaboral/Scripts/App/Services/Alert/alertService.js
@@ -0,0 +1,37 @@
+﻿var appServices = angular.module('alertRepository', []);
+appServices.factory('alertService', ['$timeout', '$rootScope',
+    function ($timeout, $rootScope) {
+        alertService = {};
+
+        alertService.add = function (type, title, msg, timeout) {
+            $rootScope.alerts.push({
+                type: type,
+                title: title,
+                msg: msg,
+                close: function () {
+                    return alertService.closeAlert(this);
+                }
+            });
+
+            if (typeof timeout == 'undefined') {
+                timeout = 4000;
+            }
+
+            if (timeout) {
+                $timeout(function () {
+                    alertService.closeAlert(this);
+                }, timeout);
+            }
+        }
+
+        alertService.closeAlert = function (alert) {
+            return this.closeAlertIdx($rootScope.alerts.indexOf(alert));
+        }
+
+        alertService.closeAlertIdx = function (index) {
+            return $rootScope.alerts.splice(index, 1);
+        }
+
+        return alertService;
+    }
+]);
