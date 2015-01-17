3a46bfb1f57255e58581c8ce2c07d6a16f29da78
clemenorellana <clemen.orellana@unitec.edu>
2015-01-16 19:58:07 -0600

    mantenimiento de vacantes por empresa

--- AccionLaboral/Scripts/App/Controllers/vacantsByCompaniesController.js
+++ AccionLaboral/Scripts/App/Controllers/vacantsByCompaniesController.js
@@ -12,42 +12,38 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
             templateUrl: '/VacantsByCompany/Create',
             controller: 'vacantsByCompaniesCtrl'
         });
+    //#/EditVacant/
 }]
 )
 .controller('vacantsByCompaniesCtrl', ['$scope', 'vacantByCompanyRepo', '$routeParams', function ($scope, vacantByCompanyRepo, $routeParams) {
 
     $scope.vacantList = [];
-    $scope.companyList = [];
-    $scope.vacantLevelList = [];
-    $scope.academicLevelList = [];
-    $scope.careerList = [];
-    $scope.cityList = [];
+    $scope.vacant_companyList = [];
+    $scope.vacant_vacantLevelList = [];
+    $scope.vacant_academicLevelList = [];
+    $scope.vacant_careerList = [];
+    $scope.vacant_cityList = [];
     $scope.load = true;
     $scope.actionContact = 'Agregar'
     $scope.contactIndex;
-    var actionCompany;
+    var actionVacant;
 
-    $scope.companyId = $routeParams.id;
+    //$scope.companyId = $routeParams.id;
+    var vacantByCompanyId = $routeParams.id;
     debugger
-    if ($scope.companyId == null) {
-        actionCompany = "add";
-        $scope.tittleCompanyForm = "Agregar datos de una Empresa";
-        $scope.buttonNameCompanyForm = "Agregar";
+    if (vacantByCompanyId == null) {
+        actionVacant = "add";
+        $scope.tittleVacantForm = "Agregar una Vacante";
+        $scope.buttonNameVacantForm = "Agregar";
     }
     else {
-        actionCompany = "edit";
-        $scope.tittleCompanyForm = "Editar datos de una Empresa";
-        $scope.buttonNameCompanyForm = "Editar";
-        var id = $scope.companyId;
+        actionVacant = "edit";
+        $scope.tittleVacantForm = "Editar una Vacante";
+        $scope.buttonNameVacantForm = "Editar";
+        //var id = $scope.companyId;
 
-        vacantByCompanyRepo.getCompany(id).success(function (data) {
-            var companyToEdit = data;
-            $scope.company_CompanyId = companyToEdit.CompanyId;
-            $scope.company_Name = companyToEdit.Name;
-            $scope.company_Area = companyToEdit.Area;
-            $scope.company_DateCreated = companyToEdit.DateCreated;
-            $scope.vacantList = companyToEdit.ContactsByCompany;
-            //$scope.company_VacantsByCompany = companyToEdit.VacantsByCompany;
+        vacantByCompanyRepo.getCompany(vacantByCompanyId).success(function (data) {
+            var vacantToEdit = data;
         });
     }
 
@@ -65,7 +61,7 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
 
     vacantByCompanyRepo.getCompanyList().success(function (data) {
         debugger
-        $scope.companyList = data;
+        $scope.vacant_companyList = data;
         $scope.totalServerItems = data.totalItems;
         $scope.items = data.items;
         $scope.load = false;
@@ -77,7 +73,7 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
         
     vacantByCompanyRepo.getVacantLevelList().success(function (data) {
         debugger
-        $scope.vacantLevelList = data;
+        $scope.vacant_vacantLevelList = data;
         $scope.totalServerItems = data.totalItems;
         $scope.items = data.items;
         $scope.load = false;
@@ -89,7 +85,7 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
 
     vacantByCompanyRepo.getAcademicLevelList().success(function (data) {
         debugger
-        $scope.academicLevelList = data;
+        $scope.vacant_academicLevelList = data;
         $scope.totalServerItems = data.totalItems;
         $scope.items = data.items;
         $scope.load = false;
@@ -101,7 +97,7 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
 
     vacantByCompanyRepo.getCareerList().success(function (data) {
         debugger
-        $scope.careerList = data;
+        $scope.vacant_careerList = data;
         $scope.totalServerItems = data.totalItems;
         $scope.items = data.items;
         $scope.load = false;
@@ -113,7 +109,7 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
 
     vacantByCompanyRepo.getCityList().success(function (data) {
         debugger
-        $scope.cityList = data;
+        $scope.vacant_cityList = data;
         $scope.totalServerItems = data.totalItems;
         $scope.items = data.items;
         $scope.load = false;
@@ -123,129 +119,60 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
         $scope.load = false;
     });
 
-    $scope.setActionContact = function (index, action) {
+
+    $scope.saveVacant = function () {
         debugger
-        if (action == 'edit') {
-            $scope.actionContact = 'Editar'
-            var contact = $scope.vacantList[index];
-            $scope.contact_ContactByCompanyId = contact.ContactByCompanyId;
-            $scope.contact_Name = contact.ContactName;
-            $scope.contact_Email = contact.ContactEmail;
-            $scope.contact_Telephone = contact.Telephone;
-            $scope.contact_Extension = contact.Extension;
-            $scope.contact_Observations = contact.Observations;
-            $scope.contact_CompanyId = contact.CompanyId;
-        }
-        else {
-            $scope.actionContact = 'Agregar'
-
-        }
-        $scope.contactIndex = index;
-
-    }
-
-    $scope.saveContactByCompany = function () {
-        debugger
-        var contact = {
-            ContactByCompanyId: $scope.contact_ContactByCompanyId,
-            ContactName: $scope.contact_Name,
-            ContactEmail: $scope.contact_Email,
-            Telephone: $scope.contact_Telephone,
-            Extension: $scope.contact_Extension,
-            Observations: $scope.contact_Observations,
-            CompanyId: $scope.contact_CompanyId
-        };
-
-        if ($scope.actionContact == 'Agregar') {
-            $scope.vacantList.push(contact);
-        }
-        else {
-            $scope.vacantList[$scope.contactIndex] = contact;
+        var newVacant = {
+            //VacantByCompanyId : 
+            VacantName : $scope.vacant_VacantName,
+            Active : 'true',
+            Quantity : $scope.vacant_Quantity,   
+            StartAge : $scope.vacant_StartAge,   
+            EndAge : $scope.vacant_EndAge,     
+            Gender : $scope.vacant_Gender,    
+            Requirements : $scope.vacant_Requirements,
+            ChargeDescription : $scope.vacant_ChargeDescription,
+            RequestDate : new Date($scope.vacant_RequestDate),
+            //CoverdDate : $scope.
+            //CompanyId: $scope.vacant_company.CompanyId,
+            //AcademicLevelId: $scope.vacant_academicLevel.AcademicLevelId,
+            //CareerId: $scope.vacant_career.CareerId,
+            //CityId: $scope.vacant_city.CityId,
+            //VacantLevelId: $scope.vacant_vacantLevel.VacantLevelId
         }
 
-        $scope.clearContactForm();
-
-    }
-
-    $scope.editContactByCompany = function (index) {
-        var contact = $scope.vacantList[index];
-        $scope.contact_Name = contact.ContactName;
-        $scope.contact_Email = contact.ContactEmail;
-        $scope.contact_Telephone = contact.Telephone;
-        $scope.contact_Extension = contact.Extension;
-        $scope.contact_Observations = contact.Observations;
-    }
-
-    $scope.clearContactForm = function () {
-        $scope.contact_Name = "";
-        $scope.contact_Email = "";
-        $scope.contact_Telephone = "";
-        $scope.contact_Extension = "";
-        $scope.contact_Observations = "";
-        $scope.actionContact = 'Agregar'
-    }
-
-    $scope.removeContactByCompany = function (index) {
-        $scope.vacantList.splice(index, 1);
-    }
-
-    $scope.saveCompany = function () {
-        debugger
-        var newCompany = {
-            CompanyId: $scope.company_CompanyId,
-            CompanyName: $scope.company_Name,
-            CompanyArea: $scope.company_Area,
-            DateCreated: $scope.company_DateCreated,
-            ContactsByCompany: [],
-            //aagregar vacantes
-        }
-
-        if ($scope.vacantList != null) {
-            for (var j = 0; j < $scope.vacantList.length; j++) {
-                var contactByCompany = $scope.vacantList[j];
-                var contact = {
-                    ContactByCompanyId: contactByCompany.ContactByCompanyId,
-                    ContactName: contactByCompany.ContactName,
-                    ContactEmail: contactByCompany.ContactEmail,
-                    Telephone: contactByCompany.Telephone,
-                    Extension: contactByCompany.Extension,
-                    Observations: contactByCompany.Observations,
-                    CompanyId: contactByCompany.CompanyId
-                };
-                newCompany.ContactsByCompany.push(contact);
-            }
-        }
+        
         //TODO: sacar los datos de las vacantes agregadas
-        if (actionCompany == "add") {
-            vacantByCompanyRepo.insertCompany(function () { }, newCompany);
+        if (actionVacant == "add") {
+            vacantByCompanyRepo.insertCompany(function () { }, newVacant);
         }
         else {
-            vacantByCompanyRepo.updateCompany(function () { }, newCompany);
+            vacantByCompanyRepo.updateCompany(function () { }, newVacant);
         }
 
-        window.location = "#/Companies";
+        window.location = "#/Vacants";
     };
 
-    $scope.company_cancelRedirect = function () {
-        window.location = "#/Companies";
+    $scope.vacant_cancelRedirect = function () {
+        window.location = "#/Vacants";
     }
 
-    $scope.company_addNewRedirect = function () {
-        window.location = "#/AddCompany";
+    $scope.vacant_addNewRedirect = function () {
+        window.location = "#/AddVacant";
     }
 
-    $scope.setCompanyToDelete = function (id) {
-        $scope.companyToDelete = id;
+    $scope.setVacantToDelete = function (id) {
+        $scope.vacantToDelete = id;
     }
 
-    $scope.deleteCompany = function (id) {
+    $scope.deleteVacant = function (id) {
         vacantByCompanyRepo.deleteCompany(function () {
         }, id);
-        $scope.companyToDelete = 0;
-        $scope.refreshCompanies();
+        $scope.vacantToDelete = 0;
+        $scope.refreshVacants();
     }
 
-    $scope.refreshCompanies = function () {
+    $scope.refreshVacants = function () {
         vacantByCompanyRepo.getCompanyList().success(function (data) {
             debugger
             $scope.vacantList = data;
@@ -259,8 +186,8 @@ angular.module("vacantsByCompaniesController", ['ngRoute', 'vacantByCompanyRepos
     });
     }
 
-    $scope.company_editRedirect = function (id) {
-        window.location = "#/EditCompany/" + id;
+    $scope.vacant_editRedirect = function (id) {
+        window.location = "#/EditVacant/" + id;
     }
 
 
